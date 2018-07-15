a028186e79e4a77fde1e625d5cf21b02f809f714
LeronDoesGaming <leronhinds@gmail.com>
2018-07-15 07:04:26 -0400

    Mini-game level loader

--- src/ZtechNetwork/HyperdrivePE.php
+++ src/ZtechNetwork/HyperdrivePE.php
@@ -2,7 +2,7 @@
 namespace ZtechNetwork;
 
 use pocketmine\plugin\PluginBase;
-use pocketmine\pligin\Plugin;
+use pocketmine\plugin\Plugin;
 use pocketmine\scheduler\PluginTask;
 use pocketmine\event\plugin\PluginEvent;
 use pocketmine\entity\Entity;
@@ -28,17 +28,17 @@ use pocketmine\level\Position;
 
 class HyperdrivePE extends PluginBase implements Listener {
 	
-	const VERSION = "1.0.0.0";
+	const NAME = "Hyperdrive PE";
+	const CODENAME = "[BETA]";
+	const VERSION = "1.0.0";
 	
 	public $prefix = C::GRAY . "[" . C::WHITE . C::BOLD . "S" . C::RED . "G" . C::RESET . C::GRAY . "] ";
 	public $mode = 0;
 	public $courses = array();
 	public $currentLevel = "";
 	
-	public function onEnable()
-	{
+	public function onEnable(): void {
 		$this->getServer()->getPluginManager()->registerEvents($this ,$this);
-		$this->getLogger()->info(C::GREEN . "HyperdrivePE has successfully loaded!");
 		$this->saveResource("config.yml");
 		@mkdir($this->getDataFolder());
 		$config = new Config($this->getDataFolder() . "/config.yml", Config::YAML);
@@ -50,8 +50,8 @@ class HyperdrivePE extends PluginBase implements Listener {
 		{
 			$this->getServer()->loadLevel($lev);
 		}
-		if($config->get("lightning_effect")==null){
-			$config->set("lightning_effect","ON");
+		if($config->get("firework_effect")==null){
+			$config->set("firwork_effect","ON");
 		}
 		$config->save();
 		$this->getServer()->getScheduler()->scheduleRepeatingTask(new GameSender($this), 20);
@@ -180,4 +180,305 @@ class HyperdrivePE extends PluginBase implements Listener {
 				return true;
 		}
 	}
+				
+	public function onInteract(PlayerInteractEvent $event)
+				{
+					$player = $event->getPlayer();
+					$block = $event->getBlock();
+					$tile = $player->getLevel()->getTile($block);
+					
+					if($tile instanceof Sign)
+					{
+						if($this->mode==26)
+						{
+							$tile->setText(C::GRAY . "[§2Join§7]",C::BLUE  . "0 / 20",$this->currentLevel,$this->prefix);
+							$this->refreshCourses();
+							$this->currentLevel = "";
+							$this->mode = 0;
+							$player->sendMessage($this->prefix . "The course has been registered successfully!");
+						}
+						else
+						{
+							$text = $tile->getText();
+							if($text[3] == $this->prefix)
+							{
+								if($text[0]==C::WHITE . "[§bJoin§f]")
+								{
+									$config = new Config($this->getDataFolder() . "/config.yml", Config::YAML);
+									$level = $this->getServer()->getLevelByName($text[2]);
+									$aop = count($level->getPlayers());
+									$thespawn = $config->get($text[2] . "Spawn" . ($aop+1));
+									$spawn = new Position($thespawn[0]+0.5,$thespawn[1],$thespawn[2]+0.5,$level);
+									$level->loadChunk($spawn->getFloorX(), $spawn->getFloorZ());
+									$player->teleport($spawn,0,0);
+									$player->getInventory()->clearAll();
+									$player->sendMessage("§7§l[§fHyper§cdrive§7] You have just joined",$this->currentLevel);
+									$player->getInventory()->setContents(array(Item::get(0, 0, 0)));
+									$player->getInventory()->setChestplate(Item::get(Item::ELYTRA));
+									$player->getInventory()->setItem(0, Item::get(Item::MAGMA_CREAM, 0, 1));
+									$player->getInventory()->setItem(0, Item::get(Item::TNT, 0, 2));
+									$player->getInventory()->sendArmorContents($player);
+									$player->getInventory()->setHotbarSlotIndex(0, 0);
+								}
+								else
+								{
+									$player->sendMessage($this->prefix . "This game is full, please try again later.");
+								}
+							}
+						}
+					}
+					else if($this->mode>=1&&$this->mode<=24)
+					{
+						$config = new Config($this->getDataFolder() . "/config.yml", Config::YAML);
+						$config->set($this->currentLevel . "Spawn" . $this->mode, array($block->getX(),$block->getY()+1,$block->getZ()));
+						$player->sendMessage($this->prefix . "Spawn " . $this->mode . " has been registered!");
+						$this->mode++;
+						if($this->mode==25)
+						{
+							$player->sendMessage($this->prefix . "Now tap on a deathmatch spawn.");
+						}
+						$config->save();
+					}
+					else if($this->mode==25)
+					{
+						$config = new Config($this->getDataFolder() . "/config.yml", Config::YAML);
+						$level = $this->getServer()->getLevelByName($this->currentLevel);
+						$level->setSpawn = (new Vector3($block->getX(),$block->getY()+1,$block->getZ()));
+						$config->set("courses",$this->courses);
+						$player->sendMessage($this->prefix . "Please tap the sign to register this hyperdrive course!");
+						$spawn = $this->getServer()->getDefaultLevel()->getSafeSpawn();
+						$this->getServer()->getDefaultLevel()->loadChunk($spawn->getFloorX(), $spawn->getFloorZ());
+						$player->teleport($spawn,0,0);
+						$config->save();
+						$this->mode=26;
+					}
+				}
+				
+	public function refreshCourses()
+				{
+					$config = new Config($this->getDataFolder() . "/config.yml", Config::YAML);
+					$config->set("courses",$this->courses);
+					foreach($this->courses as $course)
+					{
+						$config->set($course . "PlayTime", 780);
+						$config->set($course . "StartTime", 60);
+					}
+					$config->save();
+				}
+				
+	public function onDisable()
+				{
+					$this->saveResource("config.yml");
+				}
+		}
+		class RefreshSigns extends PluginTask {
+	public $prefix = C::GRAY . "[" . C::WHITE . C::BOLD . "S" . C::RED . "G" . C::RESET . C::GRAY . "] ";
+	
+	public function __construct($plugin)
+			{
+				$this->plugin = $plugin;
+				parent::__construct($plugin);
+			}
+			
+	public function onRun($tick)
+			{
+				$allplayers = $this->plugin->getServer()->getOnlinePlayers();
+				$level = $this->plugin->getServer()->getDefaultLevel();
+				$tiles = $level->getTiles();
+				foreach($tiles as $t) {
+					if($t instanceof Sign) {
+						$text = $t->getText();
+						if($text[3]==$this->prefix)
+						{
+							$aop = 0;
+							foreach($allplayers as $player){if($player->getLevel()->getFolderName()==$text[2]){$aop=$aop+1;}}
+							$ingame = C::WHITE . "[§bJoin§f]";
+							$config = new Config($this->plugin->getDataFolder() . "/config.yml", Config::YAML);
+							if($config->get($text[2] . "PlayTime")!=780)
+							{
+								$ingame = C::GRAY . "[§cRunning§7]";
+							}
+							else if($aop>=24)
+							{
+								$ingame = C::GRAY . "[§4Full§7]";
+							}
+							$t->setText($ingame,C::BLUE  . $aop . " / 10",$text[2],$this->prefix);
+						}
+					}
+				}
+			}
+		}
+		class GameSender extends PluginTask {
+	public $prefix = C::GRAY . "[" . C::WHITE . C::BOLD . "S" . C::RED . "G" . C::RESET . C::GRAY . "] ";
+	public function __construct($plugin)
+			{
+				$this->plugin = $plugin;
+				parent::__construct($plugin);
+			}
+			
+	public function onRun($tick)
+			{
+				$config = new Config($this->plugin->getDataFolder() . "/config.yml", Config::YAML);
+				$courses = $config->get("courses");
+				if(!empty($courses))
+				{
+					foreach($courses as $course)
+					{
+						$time = $config->get($course . "PlayTime");
+						$timeToStart = $config->get($course . "StartTime");
+						$levelCourse = $this->plugin->getServer()->getLevelByName($course);
+						if($levelCourse instanceof Level)
+						{
+							$playerscourse = $levelCourse->getPlayers();
+							if(count($playersCourse)==0)
+							{
+								$config->set($course . "PlayTime", 780);
+								$config->set($course . "StartTime", 60);
+							}
+							else
+							{
+								if(count($playersCourse)>=2)
+								{
+									if($timeToStart>0)
+									{
+										$timeToStart--;
+										foreach($playersCourse as $pl)
+										{
+											$level=$pl->getLevel();
+											$level->addSound(new FizzSound($pl));
+											$pl->sendPopup(C::GRAY . "Starting in " . $timeToStart . " Seconds");
+										}
+										if($timeToStart<=0)
+										{
+											foreach($playersCourse as $pl)
+											{
+												$pl->sendMessage($this->prefix . C::GREEN . "Good Luck!");}
+										}
+										$config->set($course . "StartTime", $timeToStart);
+									}
+									else
+									{
+										$aop = count($levelCourse->getPlayers());
+										if($aop==1)
+										{
+											foreach($playersCourse as $pl)
+											{
+												$spawn = $this->plugin->getServer()->getDefaultLevel()->getSafeSpawn();
+												$this->plugin->getServer()->getDefaultLevel()->loadChunk($spawn->getX(), $spawn->getZ());
+												$pl->teleport($spawn,0,0);
+											}
+											$config->set($course . "PlayTime", 780);
+											$config->set($course . "StartTime", 60);
+										}
+										$time--;
+										if($time>=180)
+										{
+											$time2 = $time - 180;
+											$minutes = $time2 / 60;
+											foreach($playersCourse as $pl)
+											{
+												$pl->sendPopup($this->prefix . $time2 . " left in the match!");
+											}
+											if(is_int($minutes) && $minutes>0)
+											{
+												foreach($playersCourse as $pl)
+												{
+													$pl->sendMessage($this->prefix . $minutes . " minutes to deathmatch");
+												}
+											}
+											else if($time2 == 300)
+											{
+												foreach($playersCourse as $pl)
+												{
+													$pl->sendMessage($this->prefix . "");
+												}
+												$this->refillChests($levelCourse);
+											}
+											else if($time2 == 30 || $time2 == 15 || $time2 == 10 || $time2 ==5 || $time2 ==4 || $time2 ==3 || $time2 ==2 || $time2 ==1)
+											{
+												foreach($playersCourse as $pl)
+												{
+													$pl->sendMessage($this->prefix . $time2 . "");
+												}
+											}
+											if($time2 <= 0)
+											{
+												$spawn = $levelCourse->getSafeSpawn();
+												$levelCourse->loadChunk($spawn->getX(), $spawn->getZ());
+												foreach($playersCourse as $pl)
+												{
+													$pl->teleport($spawn,0,0);
+												}
+											}
+										}
+										else
+										{
+											$minutes = $time / 60;
+											if(is_int($minutes) && $minutes>0)
+											{
+												foreach($playersCourse as $pl)
+												{
+													$pl->sendMessage($this->prefix . $minutes . " minutes remaining");
+												}
+											}
+											else if($time == 30 || $time == 15 || $time == 10 || $time ==5 || $time ==4 || $time ==3 || $time ==2 || $time ==1)
+											{
+												foreach($playersCourse as $pl)
+												{
+													$pl->sendMessage($this->prefix . $time . " seconds remaining");
+												}
+											}
+											if($time <= 780)
+											{
+											}
+											
+											if($time <= 0)
+											{
+												$spawn = $this->plugin->getServer()->getDefaultLevel()->getSafeSpawn();
+												$this->plugin->getServer()->getDefaultLevel()->loadChunk($spawn->getX(), $spawn->getZ());
+												foreach($playersCourse as $pl)
+												{
+													$pl->teleport($spawn,0,0);
+													$pl->sendMessage($this->prefix . "No winner this time!");
+													$pl->getInventory()->clearAll();
+												}
+												$time = 780;
+											}
+										}
+										$config->set($Course . "PlayTime", $time);
+									}
+								}
+								else
+								{
+									if($timeToStart<=0)
+									{
+										foreach($playersCourse as $pl)
+										{
+											$pl->getInventory()->clearAll();
+											$pl->sendMessage($this->prefix . C::GRAY . "You won the match!");
+											$spawn = $this->plugin->getServer()->getDefaultLevel()->getSafeSpawn();
+											$this->plugin->getServer()->getDefaultLevel()->loadChunk($spawn->getX(), $spawn->getZ());
+											$pl->teleport($spawn,0,0);
+										}
+										$config->set($course . "PlayTime", 780);
+										$config->set($course . "StartTime", 60);
+									}
+									else
+									{
+										foreach($playersCourse as $pl)
+										{
+											$pl->sendPopup(C::RED . "A game requires 2 players!");
+											
+										}
+										$config->set($course . "PlayTime", 780);
+										$config->set($course . "StartTime", 60);
+									}
+								}
+							}
+						}
+					}
+				}
+				$config->save();
+	   }
+			
 }
